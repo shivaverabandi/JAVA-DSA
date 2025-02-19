810a03ecf6f8c9a2274cb0f6b889be66a473bb77
Shiva Verabandi <vsivasai07@gmail.com>
2025-02-19 12:14:14 +0530

    added pascal's triangle variation problem

---
+++ Math/FindNcR.java
@@ -0,0 +1,24 @@
+package Math;
+
+public class FindNcR {
+    private static int findNcR(int n, int r){ // pascal triangle variation (base logic of pascal's triangle)
+        if(n == 0){
+            return 1;
+        }else if(n==r){
+            return 1;
+        }
+
+        long ans = 1;
+        for(int i = 0; i<r; i++){
+            ans = ans * (n-i);
+            ans = ans / (i+1);
+        }
+        return (int)ans;
+
+    }
+    public static void main(String[] args) {
+        int N = 4;
+        int R = 2;
+        System.out.println(findNcR(N,R));
+    }
+}
