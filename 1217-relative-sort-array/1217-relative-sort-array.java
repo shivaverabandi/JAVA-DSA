e7a41e779ca6ec703a19765fc0a4e30fef9b2af3
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-06-15 15:17:04 +0530

    Update 1217-relative-sort-array.java

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -1,25 +1,32 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         int cnt[] = new int[1001];
-        for(int i = 0; i<arr1.length; i++){
+        // count the freq of elements in arr1
+        for(int i = 0; i<arr1.length; i++){ // -> o(N)
             cnt[arr1[i]]++;
         }
-        int ans[] = new int[arr1.length];
+
+        int ans[] = new int[arr1.length]; // Output space // O(N)
         int idx = 0;
-        for(int i = 0; i<arr2.length; i++){
+        // fill the answer array according to the elements in arr2.
+        for(int i = 0; i<arr2.length; i++){ 
+            // add in ans array until count becomes 0
             while(cnt[arr2[i]] > 0 ){
                 ans[idx] = arr2[i];
                 cnt[arr2[i]]--;
                 idx++;
             }
         }
+        // fill the ans array with arr1 elements which are not in arr2.
         for(int i = 0; i < 1001; i++){
+            // add in ans array until count becomes 0
             while(cnt[i] > 0){
                 ans[idx] = i;
                 cnt[i]--;
                 idx++;
             }
         }
+        // return ans array.
         return ans;
     }
 }
