175bb93b49e25356d6adab6e91135e6e163f56d7
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-06-15 13:28:30 +0530

    Update 1044-find-common-characters.java

--- 1044-find-common-characters/1044-find-common-characters.java
+++ 1044-find-common-characters/1044-find-common-characters.java
@@ -10,21 +10,21 @@ class Solution {
             cmnCharCnt[ch - 'a']++;
         }
         // step 2 --> travese whole array from 1 to length
-        for(int i = 1; i < words.length; i++){
+        for(int i = 1; i < words.length; i++){ // Time Complexity --> O(n * k)
             Arrays.fill(currCharCnt,0);
             // step 2.1 --> count freq of chars in words[i].
-            for(char letter : words[i].toCharArray()){
+            for(char letter : words[i].toCharArray()){ // --> O(K)
                 currCharCnt[letter - 'a']++;
             }
             // step 2.2 --> update cmnCharCnt[] with min cnt of chars
-            for(int ch = 0; ch < 26; ch++){
+            for(int ch = 0; ch < 26; ch++){ // O(1)
                 cmnCharCnt[ch] = Math.min(cmnCharCnt[ch],currCharCnt[ch]);
             }
         }
 
         // step 3 --> add to ans list
         for(int idx = 0; idx < 26; idx++){
-            while(cmnCharCnt[idx] > 0){
+            while(cmnCharCnt[idx] > 0){ // o(K)
                 ans.add(String.valueOf((char)(idx + 'a'))); // I did mistake that instead of adding +a, I substract it with a.
                 cmnCharCnt[idx]--;
             }
