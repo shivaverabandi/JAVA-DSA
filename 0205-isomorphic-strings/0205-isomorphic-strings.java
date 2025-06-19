4d1927f996ed93edf1a17de63dffbc8ad7ee1f88
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-06-19 11:14:05 +0530

    Time: 9 ms (76.29%), Space: 42.9 MB (11.03%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         Map<Character,Character> smap = new HashMap<>();
-        Map<Character,Boolean> isMapped = new HashMap<>();
+        // Map<Character,Boolean> isMapped = new HashMap<>();
+        // Because we use containsValue(value) method to check is t[i] mapped earlier.
         int len = s.length();
         for(int i = 0; i<len; i++){
             Character schar = s.charAt(i);
@@ -11,11 +12,10 @@ class Solution {
                     return false;
                 }
             }else{
-                if(isMapped.containsKey(tchar) && isMapped.get(tchar)){
+                if(smap.containsValue(tchar)){
                     return false;
                 }
                 smap.put(schar,tchar);
-                isMapped.put(tchar,true);
             }
         }
         return true;
