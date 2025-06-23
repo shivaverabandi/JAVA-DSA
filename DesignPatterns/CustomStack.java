a402e93127136f1e613d14802a4c845e31f20c19
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-06-23 15:35:46 +0530

    Update CustomStack.java

--- DesignPatterns/CustomStack.java
+++ DesignPatterns/CustomStack.java
@@ -1,8 +1,28 @@
-package DesignPatterns;
 
+class DynamicStack<E> extends CustomStack<E>{
+    
+    public DynamicStack(){
+        super();
+    }
+    
+    public DynamicStack(int size){
+        super(size);
+    }
+    
+    @Override
+    public void push(E o){
+        if(isFull()){
+            Object []temp = new Object[2* this.data.length];
+            temp = Arrays.copyOf(data,temp.length);
+            data = temp;
+        }
+        super.push(o);
+    }
+    
+}
 public class CustomStack<E> {
-    private Object [] data;
-    private int top = -1;
+    public Object [] data;
+    protected int top = -1;
     private static final int DEFAULT_CAPACITY = 10;
 
     public CustomStack(){
@@ -42,28 +62,55 @@ public class CustomStack<E> {
         return top + 1;
     }
 
-    private boolean isFull(){
+    public boolean isFull(){
         return top == data.length - 1;
     }
 
 }
-
 class Main{
     public static void main(String []args){
         CustomStack<Integer> stack = new CustomStack<>();
-        stack.push(2);
-        stack.push(4);
-        stack.push(6);
-        stack.push(9);
+//         stack.push(2);
+//         stack.push(4);
+//         stack.push(6);
+//         stack.push(9);
 
-        System.out.println(stack.peek());
-        System.out.println(stack.isEmpty());
-        System.out.println(stack.pop());
+//         System.out.println(stack.peek());
+//         System.out.println(stack.isEmpty());
+//         System.out.println(stack.pop());
 
-        while(!stack.isEmpty()){
-            System.out.println(stack.pop());
+//         while(!stack.isEmpty()){
+//             System.out.println(stack.pop());
+//         }
+
+        // System.out.println(stack.pop());
+        
+        
+        CustomStack<Integer> st = new DynamicStack<>();
+
+        st.push(2);
+        st.push(4);
+        st.push(6);
+        st.push(9);
+        st.push(2);
+        st.push(4);
+        st.push(6);
+        st.push(9);
+        
+
+        st.push(2);
+        st.push(4);
+        st.push(6);
+        st.push(9);
+        st.push(2);
+        st.push(4);
+        st.push(6);
+        st.push(9);
+        System.out.println(st.size());
+        while(!st.isEmpty()){
+            System.out.println(st.peek());
+            st.pop();
         }
-
-        System.out.println(stack.pop());
+        
     }
 }
