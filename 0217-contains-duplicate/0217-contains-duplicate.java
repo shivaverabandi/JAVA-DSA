7ee1df7bbbe5e2b7ca2d5016b324b0643ffdac51
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-07-02 15:00:41 +0530

    Update 0217-contains-duplicate.java

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -9,4 +9,9 @@ class Solution {
         }
         return false;
     }
+   // Appraoch 2 this also same but using streams.
+    public boolean hasDuplicate(int[] nums) {
+        // here logic is if set length is less than nums length then there are duplicates.
+        return Arrays.stream(nums).distinct().count() < nums.length;
+    }
 }
