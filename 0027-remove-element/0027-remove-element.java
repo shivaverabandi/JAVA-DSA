53d404ef100ef6c3da940da5ece5ed33f7c67e39
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-07-03 16:05:38 +0530

    Update 0027-remove-element.java

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -9,4 +9,17 @@ class Solution {
         }
         return k;
     }
+    // Appraoch 2 . small tricky, but this is impressive
+    public int removeElement(int[] nums, int val) {
+        int idx = 0;
+        int n = nums.length;
+        while(idx < n){
+            if(nums[idx] == val){
+                nums[idx] = nums[--n];
+            }else{
+                idx++;
+            }
+        }
+        return n;
+    }
 }
