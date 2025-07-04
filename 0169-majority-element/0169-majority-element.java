7c2e47466be7395d108f8be9dcaa42309248b202
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-07-04 14:27:22 +0530

    Time: 1 ms (99.85%), Space: 53.3 MB (29.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int len = nums.length;
+        int ans = -1;
+        int cnt = 0;
+        for(int num : nums){
+            if(cnt == 0){
+                ans = num;
+                cnt = 1;
+            }else if(num != ans){
+                cnt--;
+            }else {
+                cnt++;
+            }
+        }
+        return ans;
+    }
+
+
+    public int majorityElement2(int []nums){
+        // space o(n), time o(n)
         Map<Integer,Integer> map = new HashMap<>();
         int len = nums.length;
         for(int num : nums){
