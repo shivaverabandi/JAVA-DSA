08b03391f4be60708dada2ba515e9667f3a8abdc
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-07-04 15:54:26 +0530

    Update 0049-group-anagrams.java

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        List<List<String>> ans ;
+        List<List<String>> ans = new ArrayList<>() ;
         HashMap<String, List<String>> map = new HashMap<>();
 
-        for(String s : strs){
+        for(String s : strs){ // o(N) Overall Time Complexity is O(N * S*logS)
 
-            char []chars = s.toCharArray();
-            Arrays.sort(chars);
+            char []chars = s.toCharArray(); // o(S)
+            Arrays.sort(chars); // o(s*logs) length of String S
 
-            String key = new String(chars); // converting array of chars to string.
+            String key = new String(chars); // converting array of chars to string. 
             // if the key is not seen until, then we have to create a list to store those grps of anagrams.
             if(!map.containsKey(key)){
                 map.put(key, new ArrayList<>());
@@ -19,7 +19,9 @@ class Solution {
 
         }
 
-        ans = new ArrayList<>(map.values());
+        for(List<String> list : map.values()){
+            ans.add(list);
+        }
         return ans;
 
     }
