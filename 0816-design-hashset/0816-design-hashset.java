22bcead967aac21e04ffa062d2eb8a8c724f0620
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-07-04 14:45:58 +0530

    Time: 490 ms (11.37%), Space: 54.6 MB (21.73%) - LeetHub

---
+++ 0816-design-hashset/0816-design-hashset.java
@@ -0,0 +1,44 @@
+class MyHashSet {
+
+    private ArrayList<Integer> hashBucket;
+
+    public MyHashSet() {
+        hashBucket = new ArrayList<>();
+    }
+    
+    public void add(int key) {
+        for(int num : hashBucket){ // o(N)
+            if(num == key){
+                return;
+            }
+        }
+
+        hashBucket.add(key);
+    }
+    
+    public void remove(int key) {
+        for(int i = 0; i < hashBucket.size(); i++){ // o(N)
+            if(key == hashBucket.get(i)){
+                hashBucket.remove(i);
+            }
+        }
+        
+    }
+    
+    public boolean contains(int key) { // o(N)
+        for(int num : hashBucket){
+            if(num == key){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
