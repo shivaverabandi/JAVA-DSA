821633b11bcc1a65f0c4beb2463a16d28ae4c8c4
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-07-05 15:49:51 +0530

    Time: 19 ms (19.15%), Space: 48.7 MB (14.74%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,5 +1,40 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        
+        List<List<String>> ans = new ArrayList<>();
+        Map<String, List<String>> grp = new HashMap<>();
+
+        for(String s : strs){
+
+            int []frChar = new int[26];
+            char []chArr = s.toCharArray();
+
+            for(char ch : chArr){
+                frChar[ch -'a']++;
+            }
+
+            StringBuilder sbKey = new StringBuilder();
+            for(int fr : frChar){
+                sbKey.append(fr).append('#'); // I have made mistake that will generate same hash for two diff strings
+            }
+
+            String key = sbKey.toString();
+            if(!grp.containsKey(key)){
+                grp.put(key, new ArrayList<>());
+            }
+            grp.get(key).add(s);
+
+        } 
+
+        for(List<String> list : grp.values()){
+            ans.add(list);
+        }
+        return ans;
+    }
+
+
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
 
         List<List<String>> ans = new ArrayList<>() ;
         HashMap<String, List<String>> map = new HashMap<>();
