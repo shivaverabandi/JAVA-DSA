c9a5a404d1d4079a4b31d6a10f2eb23e545d6e17
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-07-14 18:47:47 +0530

    Time: 11 ms (91.89%), Space: 49.8 MB (5.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,5 +1,42 @@
 class Solution {
+        /*Bucket Sorting --> O(N) */
     public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
+
+        // step 1 : count freq of each element
+        Map<Integer,Integer> freq = new HashMap<>();
+        for(int num : nums){
+            freq.put(num, freq.getOrDefault(num,0)+1);
+        }
+
+        //step 2 : Build Buckets for the values
+        List<Integer> []freqBuckets = new ArrayList[n+1]; // why n+1 ? at most element can appear n times 
+        // fill the each index with empty list
+        for(int i = 0; i < n+1; i++){
+            freqBuckets[i] = new ArrayList<>();
+        }
+
+        // step 3 : Iterate on map to fill buckets as per their values
+        for(Map.Entry<Integer,Integer> set : freq.entrySet()){
+            int count = set.getValue();
+            int number = set.getKey(); 
+            freqBuckets[count].add(number);
+        }
+
+        //step 4 :  get the top k elements by iterating the buckets from back 
+        int ans[] = new int[k];
+        int index = 0;
+        for(int i = freqBuckets.length-1; i > 0 && index < k; i--){ // why i > 0 ? because 0 index means count is 0. so no need to check.
+            for(int number : freqBuckets[i]){
+                ans[index++] = number;
+                if(index == k)
+                    return ans;
+            }
+        }
+        return ans;
+    }
+    /* Min Heap + Freq counting Approach */
+    public int[] topKFrequent2(int[] nums, int k) {
         int n = nums.length;
         int []ans = new int[k];
 
@@ -26,7 +63,7 @@ class Solution {
 
 
     /* Freq Counting + Sorting based on Freq of element */
-    public int[] topKFrequent2(int[] nums, int k) { // O(N*LogN)
+    public int[] topKFrequent3(int[] nums, int k) { // O(N*LogN)
         int n = nums.length;
 
         // step 1 count the freq of each element
