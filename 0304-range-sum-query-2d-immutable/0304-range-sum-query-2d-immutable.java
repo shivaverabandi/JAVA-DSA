174f34b2d8f4eef14be0bf8a5fe8135efcbd78d7
SHIVA VERABANDI <166308619+shivaverabandi@users.noreply.github.com>
2025-07-17 15:29:21 +0530

    Time: 121 ms (16.73%), Space: 71.3 MB (19.28%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,10 +1,51 @@
 class NumMatrix {
     private int [][]matrix;
+    int m; // rows
+    int n; // columns
+
+    private int [][]prefix; // prefix array
+
     public NumMatrix(int[][] matrix) {
         this.matrix = matrix;
+
+        m = matrix.length;
+        n = matrix[0].length;
+
+        // calculating prefix for each row
+        prefix = new int[m][n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(j == 0){
+                    prefix[i][j] = matrix[i][j];
+                }else{
+                    prefix[i][j] = prefix[i][j-1] + matrix[i][j];
+                }
+            }
+        }
     }
     
+    // 1D Prefix --> O(M)
     public int sumRegion(int row1, int col1, int row2, int col2) {
+        int ans = 0;
+        
+        for(int row = row1; row <= row2; row++){
+            // prefix formulae --> sum(L,R) => prefix[R] - prefix[L-1]
+
+            if(col1 > 0){
+                ans += prefix[row][col2] - prefix[row][col1 - 1];
+            }else{
+                ans += prefix[row][col2];
+            }
+        }
+        return ans;
+    }
+
+
+
+
+    // Brute Force solution
+    public int sumRegion2(int row1, int col1, int row2, int col2) { // O(n*m)
         int ans = 0;
         for(int i = row1; i <= row2; i++){
             for(int j = col1; j <= col2; j++){
